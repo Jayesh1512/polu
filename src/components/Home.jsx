f7ecd0d2b737bcae5cd3d4fce187d818201de2e7
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2024-01-26 00:05:43 +0530

    reponsive first page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,13 +8,13 @@ function Home() {
   return (
     <>
     <div className='flex justify-center'>
-      <img src={dextertitle} alt="" className='w-[40vw] '/>    
+      <img src={dextertitle} alt="" className='lg:w-[30vw] w-[80vw] lg:py-0 py-20'/>    
     </div>
     <div className='flex justify-center p-2'>
-    <img src={regbutton} alt="" className='w-[15vw] '/>    
+    <img src={regbutton} alt="" className='lg:w-[15vw] w-[50vw] py-4'/>    
     </div>
     <div className='flex justify-center'>
-    <img src={dexterlog} alt="" className='w-[20vw]'/>    
+    <img src={dexterlog} alt="" className='lg:w-[25vw] w-[80vw] lg:py-0 py-14'/>    
     </div>
     </>
   )

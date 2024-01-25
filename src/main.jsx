f9794ee722b6356f83873afa59aed8cc85b8e514
Mefisto04 <mayurkawale0000@gmail.com>
2024-01-26 00:18:05 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Page3 from './components/Page3.jsx'
 import './index.css'
-import Login from './components/Login.jsx'
+// import Login from './components/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Page3 />
   </React.StrictMode>,
 )

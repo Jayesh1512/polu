c33a21d48d97da3f4d90c68c0d1fc8a4afb354b4
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2024-01-25 23:37:12 +0530

    front page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import dextertitle from '../assets/dextertitle.svg'
+import regbutton from '../assets/regbutton.svg'
+import dexterlog from '../assets/dexterlog.svg'
+
 
 function Home() {
   return (
-    <div className='bg-slate-700 w-screen h-full'>
-      
+    <>
+    <div className='flex justify-center'>
+      <img src={dextertitle} alt="" className='w-[40vw] '/>    
     </div>
+    <div className='flex justify-center p-2'>
+    <img src={regbutton} alt="" className='w-[15vw] '/>    
+    </div>
+    <div className='flex justify-center'>
+    <img src={dexterlog} alt="" className='w-[20vw]'/>    
+    </div>
+    </>
   )
 }
 

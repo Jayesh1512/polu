1faabbd631c4ed4ea9c38beaf7b4f3ef872e420c
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2024-01-27 15:43:25 +0530

    Update Page3.jsx

--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -10,7 +10,7 @@ const Page3 = () => {
         <img src={About} alt="" className="w-full md:w-1/2 object-cover rounded-md mb-4 md:mb-0" />
         <div className="flex flex-col justify-center text-center md:w-1/2 md:ml-4">
           <p className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">About The Event</p>
-          <p className="text-lg md:text-xl lg:text-2xl p-4">
+          <p className="text-lg md:text-xl lg:text-2xl p-4 font-semibold">
             Dexter’s Laboratory Adventure is an engaging coding event featuring Mandark’s 
           Aptitude Challenge and Dexter’s Prompt Mishaps in Round 1. Teams earn points
            based on aptitude and prompt accuracy. Round 2, Code Relay, involves coding Dexter’s 

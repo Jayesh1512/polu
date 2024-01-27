bd837b55171a59908cff1ea3725d26578368daaf
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2024-01-27 11:26:21 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className='bg-cover bg-center h-screen'  style={{ backgroundImage: `url(${backgroundImage})` }}>
     <div className='flex justify-center' >
-      <img src={dextertitle} alt="" className='lg:w-[30vw] w-[80vw] lg:py-2 pt-40 mt-10'/>    
+      <img src={dextertitle} alt="" className='lg:w-[30vw] w-[80vw] lg:py-2 pt-28 mt-10'/>    
     </div>
     <div className='flex justify-center p-2'>
     <a href = "https://docs.google.com/forms/d/e/1FAIpQLSf2nx0bCKlK12hJF5GKsCf-kZ36h52X3S9m1rARjoWZVJwxDw/formrestricted?pli=1" ><img src={regbutton} alt="" className='lg:w-[15vw] w-[50vw] lg:py-5 py-10'/>  </a>  

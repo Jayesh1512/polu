2f754b107b63884f4ef8d8dc12dc0deb64a8ae7e
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2024-01-27 11:40:24 +0530

    Update Page3.jsx

--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -6,11 +6,11 @@ const Page3 = () => {
   return (
     <div className="bg-cover bg-center h-screen w-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="flex justify-center items-center">
-      <div className="md:bg-white w-full md:w-3/4 h-[80vh] lg:w-1/2 xl:w-2/3  flex flex-col md:flex-row p-4">
+      <div className="bg-white w-full md:w-3/4 h-[80vh] lg:w-1/2 xl:w-2/3  flex flex-col md:flex-row p-4">
         <img src={About} alt="" className="w-full md:w-1/2 object-cover rounded-md mb-4 md:mb-0" />
         <div className="flex flex-col justify-center text-center md:w-1/2 md:ml-4">
           <p className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">About The Event</p>
-          <p className="text-lg md:text-xl lg:text-2xl">
+          <p className="text-lg md:text-xl lg:text-2xl p-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>

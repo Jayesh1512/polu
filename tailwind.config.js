4656f26d3a234e0d76c302981967d1a8a03b876a
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2024-01-27 16:08:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       backgroundColor: {
         'card': 'rgba(68, 149, 170, 1)',
+        'primary': 'rgba(68, 149, 170, 1)',
       },
       colors:{
         'cardTitle': 'rgba(5, 61, 76, 1)',
